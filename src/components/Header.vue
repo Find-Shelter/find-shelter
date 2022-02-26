9d9a895299fdcef03fd312773257ed91a8fe25f9
Fabien Rahaingo <fabrahaingo@hotmail.fr>
2022-02-26 15:41:44 +0100

    first push

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+   <header>
+      <router-link to="/">Find Shelter</router-link>
+   </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+   margin: 10px 0 15px 5px;
+}
+
+a {
+   position: relative;
+   color: white;
+   text-decoration: none;
+   font-weight: 700;
+   font-size: 1.4rem;
+
+   &:after {
+      position: absolute;
+      content: '';
+      width: 100%;
+      height: 4px;
+      bottom: 0;
+      left: 0;
+      background-color: white;
+   }
+}
+</style>
---
+++ src/views/Legal.vue
@@ -0,0 +1,5 @@
+<template>
+   <div>
+      <h1>Partie Légale</h1>
+   </div>
+</template>
