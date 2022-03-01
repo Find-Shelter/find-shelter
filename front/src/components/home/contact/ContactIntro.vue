4351f5f9d33e376a4be84e7c4d754f9d2addc000
Antoine Bollinger <antoine.bollinger@gmail.com>
2022-03-01 13:38:13 +0100

    Some french spelling correction

--- front/src/components/home/contact/ContactIntro.vue
+++ front/src/components/home/contact/ContactIntro.vue
@@ -1,42 +1,42 @@
 <template>
-	<div id="intro_text">
-		<h1>Proposez un hébergement</h1>
-		<p>
-			Remplissez ce formulaire pour poposer une (ou plusieurs) chambres aux
-			réfugiés qui en ont besoin. 🤝
-		</p>
-		<p>
-			Vos données ne seront partagées qu'aux associations qui pourront vos
-			mettre en relation avec des personnes réfugiées. Vous pouvez trouver la
-			liste des ces associations
-			<router-link to="/legal">ici</router-link>.
-		</p>
-	</div>
+  <div id="intro_text">
+    <h1>Proposez un hébergement</h1>
+    <p>
+      Remplissez ce formulaire pour proposer une (ou plusieurs) chambres aux
+      réfugiés qui en ont besoin. 🤝
+    </p>
+    <p>
+      Vos données ne seront partagées qu'aux associations qui pourront vos
+      mettre en relation avec des personnes réfugiées. Vous pouvez trouver la
+      liste de ces associations
+      <router-link to="/legal">ici</router-link>.
+    </p>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 #intro_text {
-	max-width: 700px;
-	margin: 40px auto;
+  max-width: 700px;
+  margin: 40px auto;
 
-	h1 {
-		margin: 10px auto;
-		font-size: 2.3rem;
-	}
+  h1 {
+    margin: 10px auto;
+    font-size: 2.3rem;
+  }
 
-	> p {
-		color: grey;
-		font-size: 1.1rem;
-		margin: 10px auto;
-		text-align: left;
-	}
+  > p {
+    color: grey;
+    font-size: 1.1rem;
+    margin: 10px auto;
+    text-align: left;
+  }
 
-	@media (max-width: 600px) {
-		margin: 20px auto;
+  @media (max-width: 600px) {
+    margin: 20px auto;
 
-		> p {
-			padding: 0 5px;
-		}
-	}
+    > p {
+      padding: 0 5px;
+    }
+  }
 }
 </style>
