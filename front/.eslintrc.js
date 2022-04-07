35b168c9bfef92d358eb2bf51e17b5c8fe630132
elliotwutingfeng <wu.tingfeng@u.nus.edu>
2022-04-07 22:49:50 +0800

    version bump (#25)
    bump package versions

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
-   env: {
-      node: true,
-   },
-   extends: [
-      // add more generic rulesets here, such as:
-      'eslint:recommended',
-      'plugin:vue/vue3-essential',
-      'plugin:vue/essential'
-      // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
-   ],
-   rules: {
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
-   }
-}
+    env: {
+        node: true,
+    },
+    extends: [
+        // add more generic rulesets here, such as:
+        "eslint:recommended",
+        "plugin:vue/vue3-essential",
+        "plugin:vue/essential",
+        // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+    ],
+    rules: {
+        "vue/multi-word-component-names": "off",
+        // override/add rules settings here, such as:
+        // 'vue/no-unused-vars': 'error'
+    },
+};
